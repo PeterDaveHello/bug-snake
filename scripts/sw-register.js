d8c88f202329acf47b942b685c33fbf69f427d07
Peter Dave Hello <hsu@peterdavehello.org>
2026-03-04 00:12:33 +0800

    Improve service worker registration logging
    Report service worker lifecycle state during registration instead of always assuming immediate activation.

--- scripts/sw-register.js
+++ scripts/sw-register.js
@@ -4,7 +4,39 @@ if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker
       .register('./sw.js')
-      .then(() => console.log('SW registered'))
+      .then((reg) => {
+        const logState = () => {
+          if (reg.active) {
+            // eslint-disable-next-line no-console
+            console.log('SW registration succeeded: active');
+            return;
+          }
+
+          if (reg.waiting) {
+            // eslint-disable-next-line no-console
+            console.log('SW registration state: waiting');
+            return;
+          }
+
+          if (reg.installing) {
+            // eslint-disable-next-line no-console
+            console.log('SW registration state: installing');
+            return;
+          }
+
+          // eslint-disable-next-line no-console
+          console.log('SW registration succeeded');
+        };
+
+        logState();
+
+        const pendingWorker = reg.waiting || reg.installing;
+        if (pendingWorker) {
+          pendingWorker.addEventListener('statechange', () => {
+            logState();
+          });
+        }
+      })
       .catch((err) => {
         // eslint-disable-next-line no-console
         console.log('SW registration failed:', err);
